b14b7b4ef289438e66d3e96ab6892279e4151837
hugo <hugo@enclave.vn>
2017-09-07 13:55:33 +0700

    Functional test now checks we can input a to-do item

--- test/functional_test.py
+++ test/functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import os
 import unittest
 
@@ -10,14 +12,30 @@ class TodoTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_start(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         
-        self.assertIn('Django', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         # self.fail('Finish the test')
 
-    def test_equal(self):
-        self.assertEqual(3,3)
+        inputbox.send_keys('Buy some tea')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy some tea' for row in rows)
+        )
+
+        self.fail('Finish the test')
 
 
 if __name__ == '__main__':
