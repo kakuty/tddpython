eb32ce841d2524f79ce13fd95ea3670fc8fe9e5e
hugo <hugo@enclave.vn>
2017-09-07 16:15:31 +0700

    Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    
+    text = models.TextField(default='')
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
