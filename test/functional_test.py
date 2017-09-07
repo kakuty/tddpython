636b4a10cfde9010a1bba4b9abb80e5cc9953141
hugo <hugo@enclave.vn>
2017-09-07 16:59:02 +0700

    Redirect after POST and show all items in template

--- test/functional_test.py
+++ test/functional_test.py
@@ -24,7 +24,11 @@ class TodoTest(unittest.TestCase):
         # self.fail('Finish the test')
 
         inputbox.send_keys('Buy some tea')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
