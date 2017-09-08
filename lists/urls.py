d4674d89ddca3952b911cd2f33ac20564d353197
hugo <hugo@enclave.vn>
2017-09-08 11:49:53 +0700

    new URL, view and template to display lists

--- lists/urls.py
+++ lists/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = []
+urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world', views.view_list, name='view_list'),
 ]
