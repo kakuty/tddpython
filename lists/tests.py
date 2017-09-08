09c27ba4d6978e89bd59557ca8ae277bda292bf1
hugo <hugo@enclave.vn>
2017-09-08 12:46:34 +0700

    new list

--- lists/tests.py
+++ lists/tests.py
@@ -13,28 +13,11 @@ class HomepageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'new_item': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
 
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'new_item': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
-
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
-    
 class ListViewTest(TestCase):
 
     def test_use_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
 
 
@@ -46,6 +29,19 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'item1')
         self.assertContains(response, 'item2')
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'new_item': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'new_item': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -3,14 +3,13 @@ from django.http import HttpResponse
 from .models import Item
 
 
-def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST.get('new_item', '')
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world')
-    
+def home_page(request): 
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['new_item'])
+    return redirect('/lists/the-only-list-in-the-world/')
