6c6aee4534785b7771057f07661e1c3d9508aabe
lazidoca <im2.la.zzzzzy@gmail.com>
2017-09-12 17:14:23 +0700

    add jquery, qunit tests, list.js with keypress listeners

--- functional_test/test_list_item_validation.py
+++ functional_test/test_list_item_validation.py
@@ -6,7 +6,7 @@ import time
 class ItemValidationTest(FunctionalTest):
 
     def get_error_element(self):
-        return self.browser.find_element_by_id('.has-error')
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
